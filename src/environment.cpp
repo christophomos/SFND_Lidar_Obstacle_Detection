984fd602a7913e1c2411345aa51c83c5f754eba2
Chris Clark <chris@qalang.com>
2020-09-16 22:37:38 +0800

    Create and render lidar

--- src/environment.cpp
+++ src/environment.cpp
@@ -45,10 +45,13 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     bool renderScene = true;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
-    // TODO:: Create lidar sensor 
+    // Create lidar sensor 
+    Lidar* lidar = new Lidar(cars, 0);
 
     // TODO:: Create point processor
-  
+    pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud = lidar->scan();
+    renderRays(viewer, lidar->position, point_cloud);
+
 }
 
 
